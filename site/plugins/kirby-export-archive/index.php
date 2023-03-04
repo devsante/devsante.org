77f0b4adccf070c9ce361652a021e9cb1873b3c4
Matthieu Bergel <hello@matthieubergel.org>
2023-03-04 23:41:24 +0300

    style: prettify

--- site/plugins/kirby-export-archive/index.php
+++ site/plugins/kirby-export-archive/index.php
@@ -1,107 +1,116 @@
 <?php
 
 Kirby::plugin("mlbrgl/kirby-export-archive", [
-  "routes" => [
-    [
-      "pattern" => "export-archive",
-      "action" => function () {
-        // Copy the content/articles/ directory to archive/articles/ in an indempotent way
-        exec(
-          "rm -r archive/articles ; cp -rf content/articles/. archive/articles"
-        );
-        exec("rm -r archive/articles/*/article.txt");
-        exec("rm -r archive/articles/articles-list.txt");
+    "routes" => [
+        [
+            "pattern" => "export-archive",
+            "action" => function () {
+                // Copy the content/articles/ directory to archive/articles/ in an indempotent way
+                exec(
+                    "rm -r archive/articles ; cp -rf content/articles/. archive/articles"
+                );
+                exec("rm -r archive/articles/*/article.txt");
+                exec("rm -r archive/articles/articles-list.txt");
 
-        // Copy the content/actualites/ directory to archive/actualites/ in an indempotent way
-        exec(
-          "rm -r archive/actualites ; cp -rf content/actualites/. archive/actualites"
-        );
-        exec("rm -r archive/actualites/*/news.txt");
-        exec("rm -r archive/actualites/news-list.txt");
+                // Copy the content/actualites/ directory to archive/actualites/ in an indempotent way
+                exec(
+                    "rm -r archive/actualites ; cp -rf content/actualites/. archive/actualites"
+                );
+                exec("rm -r archive/actualites/*/news.txt");
+                exec("rm -r archive/actualites/news-list.txt");
 
-        // Remove the datetime prefix in the subdirectory names. The folder
-        // names are used as slugs by Honkit.
+                // Remove the datetime prefix in the subdirectory names. The folder
+                // names are used as slugs by Honkit.
 
-        // Note: there are collisions in the target folder names when removing
-        // the datetime prefix. This means two articles are candidate for the
-        // same slug. By listing in reverse order, we ensure that the latest
-        // article (probably an udpated version of the original article) is the
-        // one that is kept (which is what is happening on Kirby's side as
-        // well).
-        exec("cd archive/articles && ls -r | rename 's/\d{12}_//'");
-        // Remove the colliding folders that weren't renamed
-        exec("cd archive/articles && ls | grep '^[0-9]\{12\}' | xargs rm -r");
+                // Note: there are collisions in the target folder names when removing
+                // the datetime prefix. This means two articles are candidate for the
+                // same slug. By listing in reverse order, we ensure that the latest
+                // article (probably an udpated version of the original article) is the
+                // one that is kept (which is what is happening on Kirby's side as
+                // well).
+                exec("cd archive/articles && ls -r | rename 's/\d{12}_//'");
+                // Remove the colliding folders that weren't renamed
+                exec(
+                    "cd archive/articles && ls | grep '^[0-9]\{12\}' | xargs rm -r"
+                );
 
-        // Same with actualites, but with sequential numbers instead of dates
-        exec("cd archive/actualites && ls -r | rename 's/\d+_//'");
+                // Same with actualites, but with sequential numbers instead of dates
+                exec("cd archive/actualites && ls -r | rename 's/\d+_//'");
 
-        $articles = page("articles")
-          ->children()
-          ->listed()
-          ->flip();
+                $articles = page("articles")
+                    ->children()
+                    ->listed()
+                    ->flip();
 
-        $actualites = page("actualites")
-          ->children()
-          ->listed()
-          ->flip();
+                $actualites = page("actualites")
+                    ->children()
+                    ->listed()
+                    ->flip();
 
-        $pages = $articles->merge($actualites)->sortBy("datetime", "desc");
+                $pages = $articles
+                    ->merge($actualites)
+                    ->sortBy("datetime", "desc");
 
-        $summary = [
-          "## Archive devsante.org (1976 - 2022)",
-          "- [Avant-propos](INTRO.md)",
-        ];
+                $summary = [
+                    "## Archive devsante.org (1976 - 2022)",
+                    "- [Avant-propos](INTRO.md)",
+                ];
 
-        // Transform the content of each page into a standard Markdown file,
-        // using standard frontmatter, and removing kirbytext.
-        foreach ($pages as $page) {
-          $frontmatterFields = [
-            "title" => $page->title()->value(),
-            "author" => $page->author()->value(),
-            "date" => $page->datetime()->toDate("%Y-%m-%d"),
-          ];
-          $teaser = !empty($page->teaser()->value())
-            ? '<div class="teaser">' . $page->teaser()->kirbytext() . "</div>"
-            : null;
-          $frontmatter = arrayToFrontmatter($frontmatterFields);
-          $text = kirbytextToMarkdown($page->text()->value());
-          $page_path = "{$page->parent()->slug()}/{$page->slug()}";
+                // Transform the content of each page into a standard Markdown file,
+                // using standard frontmatter, and removing kirbytext.
+                foreach ($pages as $page) {
+                    $frontmatterFields = [
+                        "title" => $page->title()->value(),
+                        "author" => $page->author()->value(),
+                        "date" => $page->datetime()->toDate("%Y-%m-%d"),
+                    ];
+                    $teaser = !empty($page->teaser()->value())
+                        ? '<div class="teaser">' .
+                            $page->teaser()->kirbytext() .
+                            "</div>"
+                        : null;
+                    $frontmatter = arrayToFrontmatter($frontmatterFields);
+                    $text = kirbytextToMarkdown($page->text()->value());
+                    $page_path = "{$page->parent()->slug()}/{$page->slug()}";
 
-          file_put_contents(
-            "archive/$page_path/index.md",
-            join(
-              "\n\n",
-              array_filter([$frontmatter, $teaser, $text], function ($value) {
-                return !empty($value);
-              })
-            )
-          );
+                    file_put_contents(
+                        "archive/$page_path/index.md",
+                        join(
+                            "\n\n",
+                            array_filter(
+                                [$frontmatter, $teaser, $text],
+                                function ($value) {
+                                    return !empty($value);
+                                }
+                            )
+                        )
+                    );
 
-          $summary[] = "- [{$page->title()->value()}]($page_path/index.md)";
-        }
+                    $summary[] = "- [{$page->title()->value()}]($page_path/index.md)";
+                }
 
-        // Add a summary page to the book
-        file_put_contents(
-          "archive/SUMMARY.md",
-          join("\n", ["# Summary", ...$summary])
-        );
-      },
+                // Add a summary page to the book
+                file_put_contents(
+                    "archive/SUMMARY.md",
+                    join("\n", ["# Summary", ...$summary])
+                );
+            },
+        ],
     ],
-  ],
 ]);
 
 function arrayToFrontmatter($array)
 {
-  $frontmatter = "---\n";
-  foreach ($array as $key => $value) {
-    if (empty($value)) {
-      continue;
+    $frontmatter = "---\n";
+    foreach ($array as $key => $value) {
+        if (empty($value)) {
+            continue;
+        }
+        $escapedValue = addcslashes($value, "\"");
+        $frontmatter .= "$key: \"$escapedValue\"\n";
     }
-    $escapedValue = addcslashes($value, "\"");
-    $frontmatter .= "$key: \"$escapedValue\"\n";
-  }
-  $frontmatter .= "---";
-  return $frontmatter;
+    $frontmatter .= "---";
+    return $frontmatter;
 }
 
 // convert kirbytext images to markdown and start them on a new line. Images
@@ -109,52 +118,54 @@ function arrayToFrontmatter($array)
 // new line, but that's ok.
 function kirbytextImageToMarkdown($kirbytext)
 {
-  return preg_replace(
-    "/^(.*?)\(image:\s*(.*?)\)/mu",
-    "$1\n![]($2)",
-    $kirbytext
-  );
+    return preg_replace(
+        "/^(.*?)\(image:\s*(.*?)\)/mu",
+        "$1\n![]($2)",
+        $kirbytext
+    );
 }
 
 function kirbytextNewlineToMarkdown($kirbytext)
 {
-  return preg_replace(
-    // add two spaces at the end a line to force a newline in markdown (handled
-    // previously by kirbytext). This captures the vast majority of legitimate
-    // cases where we would want a visual new line (i.e. sentences finishing
-    // with a ".", followed by a single line feed in code), but leaves out some
-    // very rare and questionable cases, which are often best rewritten as
-    // lists.
-    "/\. ?\n([^\n])/",
-    ".  \n$1",
-    $kirbytext
-  );
+    return preg_replace(
+        // add two spaces at the end a line to force a newline in markdown (handled
+        // previously by kirbytext). This captures the vast majority of legitimate
+        // cases where we would want a visual new line (i.e. sentences finishing
+        // with a ".", followed by a single line feed in code), but leaves out some
+        // very rare and questionable cases, which are often best rewritten as
+        // lists.
+        "/\. ?\n([^\n])/",
+        ".  \n$1",
+        $kirbytext
+    );
 }
 
 // Make sure <table> tags start on a new line
 function kirbytextTableOpenToMarkdown($kirbytext)
 {
-  return preg_replace("/^(.+?)<table>/m", "$1\n<table>", $kirbytext);
+    return preg_replace("/^(.+?)<table>/m", "$1\n<table>", $kirbytext);
 }
 
 // Add a extra new line after closing </table> tags to get them recognized
 function kirbytextTableCloseToMarkdown($kirbytext)
 {
-  return preg_replace("/<\/table>\n([^\n])/", "</table>\n\n$1", $kirbytext);
+    return preg_replace("/<\/table>\n([^\n])/", "</table>\n\n$1", $kirbytext);
 }
 
 function kirbytextNumberedListToMarkdown($kirbytext)
 {
-  return preg_replace("/^(\d+)- /m", "$1. ", $kirbytext);
+    return preg_replace("/^(\d+)- /m", "$1. ", $kirbytext);
 }
 
 function kirbytextToMarkdown($kirbytext)
 {
-  return kirbytextNewlineToMarkdown(
-    kirbytextNumberedListToMarkdown(
-      kirbytextTableOpenToMarkdown(
-        kirbytextTableCloseToMarkdown(kirbytextImageToMarkdown($kirbytext))
-      )
-    )
-  );
+    return kirbytextNewlineToMarkdown(
+        kirbytextNumberedListToMarkdown(
+            kirbytextTableOpenToMarkdown(
+                kirbytextTableCloseToMarkdown(
+                    kirbytextImageToMarkdown($kirbytext)
+                )
+            )
+        )
+    );
 }
